90a53a337c1f037a0f62e85dfa2e3069246d612b
pravindhonde77 <pravindhonde77@gmail.com>
2023-02-18 17:46:08 +0530

    data

--- server/controller/todo-controller.js
+++ server/controller/todo-controller.js
@@ -29,4 +29,47 @@ const getAllTodos = async (req, res) => {
     }
 }
 
-export  {addTodo,getAllTodos}
+const toggleTodoDone = async (req, res) => {
+    try {
+      const todoRef=  await Todo.findById(req.params.id)
+      const todo=await Todo.findOneAndUpdate(
+        {_id:req.params.id},
+        {done:!todoRef.done}
+      )
+
+      await todo.save()
+        return res.status(200).json(todo)
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+
+
+const updateTodo = async (req, res) => {
+    try {
+     
+      await Todo.findOneAndUpdate(
+        {_id:req.params.id},
+        {data:req.body.data}
+      )
+
+      const todo=await Todo.findById(req.params.id)
+        return res.status(200).json(todo)
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+
+const deleteTodo = async (req, res) => {
+    try {
+      const todo=await Todo.findByIdAndDelete(req.params.id)
+        return res.status(200).json(todo)
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+
+export { addTodo, getAllTodos,toggleTodoDone ,updateTodo,deleteTodo}
